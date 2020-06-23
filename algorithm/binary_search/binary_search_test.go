51d747e712d12beb25b5c019b2b9d14390abd992
shijl <shijl@51cto.com>
2020-06-23 06:17:28 +0000

    perfedt binarySearch unit test

--- algorithm/binary_search/binary_search_test.go
+++ algorithm/binary_search/binary_search_test.go
@@ -2,48 +2,42 @@ package binary_search
 
 import "testing"
 
-func TestBinarySearch(t *testing.T) {
-	a := []int{1, 3, 5, 7, 77}
-	target := 5
-	expect := 2
+var (
+	slice  = []int{1, 3, 5, 7, 77}
+	target = 5
+	expect = 2
+)
 
-	index := binarySearch(a, target, 0, len(a)-1)
+func Test_BinarySearch(t *testing.T) {
+	index := binarySearch(slice, target, 0, len(slice)-1)
 	if index != expect {
 		t.Fatal("error")
 	}
 	t.Log(index)
 }
 
-func TestBinarySearch2(t *testing.T) {
-	a := []int{1, 3, 5, 7, 77}
-	target := 5
-	expect := 2
-
-	index := binarySearch2(a, target, 0, len(a)-1)
+func Test_BinarySearch2(t *testing.T) {
+	index := binarySearch2(slice, target, 0, len(slice)-1)
 	if index != expect {
 		t.Fatal("error")
 	}
 	t.Log(index)
 }
 
-func BenchmarkBinarySearch(b *testing.B) {
+func Benchmark_BinarySearch(b *testing.B) {
 	b.ResetTimer()
 	b.ReportAllocs()
 
-	a := []int{1, 3, 5, 7, 77}
-	target := 5
 	for i := 0; i < b.N; i++ {
-		binarySearch(a, target, 0, len(a)-1)
+		binarySearch(slice, target, 0, len(slice)-1)
 	}
 }
 
-func BenchmarkBinarySearch2(b *testing.B) {
+func Benchmark_BinarySearch2(b *testing.B) {
 	b.ResetTimer()
 	b.ReportAllocs()
 
-	a := []int{1, 3, 5, 7, 77}
-	target := 5
 	for i := 0; i < b.N; i++ {
-		binarySearch2(a, target, 0, len(a)-1)
+		binarySearch2(slice, target, 0, len(slice)-1)
 	}
 }
