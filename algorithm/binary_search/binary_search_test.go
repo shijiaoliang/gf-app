38f226c657cd4f1c9a431803bddfe6c5f6e5f0fc
shijl <shijl@51cto.com>
2020-06-23 05:52:01 +0000

    add binary_search test case

---
+++ algorithm/binary_search/binary_search_test.go
@@ -0,0 +1,25 @@
+package binary_search
+
+import "testing"
+
+func TestBinarySearch(t *testing.T) {
+	a := []int{1, 3, 5, 7, 77}
+	target := 5
+	expect := 2
+
+	index := binarySearch(a, target, 0, len(a)-1)
+	if index != expect {
+		t.Fatal("error")
+	}
+	t.Log(index)
+}
+
+func BenchmarkBinarySearch(b *testing.B) {
+	b.ResetTimer()
+	b.ReportAllocs()
+	a := []int{1, 3, 5, 7, 77}
+	target := 5
+	for i := 0; i < b.N; i++ {
+		binarySearch(a, target, 0, len(a)-1)
+	}
+}
